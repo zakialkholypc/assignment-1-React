9cd55bdb043287b74073e325566f11906f4e7d1b
zaki alkholypc <zakialkholypc@gmail.com>
2025-01-12 21:31:43 +0200

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const router = createHashRouter([
     path: "",
     element: <Layout />,
     children: [
-      { path: "react-last/", element: <Home /> },
+      { path: "", element: <Home /> },
       { path: "home", element: <Home /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
